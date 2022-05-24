f81a010c858b26aa3bd62b32e818d937240ca88c
AnwarHossainSR <mahedianwar@gmail.com>
2022-05-24 23:20:32 +0600

    user Registration

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB: "mongodb://localhost:27017/graphql-node-js",
-}
+  MONGODB: "mongodb://localhost:27017/graphql-node-js",
+  SECRET_KEY: "dsfdsf5dsfFGhg$$#hgjhg%ghfhghhghgvh4566GHJYT$fg#f@hjj",
+};
