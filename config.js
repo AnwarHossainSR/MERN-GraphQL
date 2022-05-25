2ddfcf61633bfbbc23674df498a8da8e7b5169cd
AnwarHossainSR <mahedianwar@gmail.com>
2022-05-25 19:57:54 +0600

    Api created with GraphQL

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  MONGODB: "mongodb://localhost:27017/graphql-node-js",
+  //MONGODB: "mongodb://localhost:27017/graphql-node-js",
+  MONGODB:
+    "mongodb+srv://user:RktkkTGK2dcU5DA7@cluster0.yrm1z.mongodb.net/mern-graphql?retryWrites=true&w=majority",
   SECRET_KEY: "dsfdsf5dsfFGhg$$#hgjhg%ghfhghhghgvh4566GHJYT$fg#f@hjj",
 };
