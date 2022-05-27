067944d99562b3a01a61a7b4e89fd19e1b890044
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-05-27 09:14:56 +0600

    implement getPosts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
 import "./App.css";
 import MenuBar from "./components/MenuBar";
@@ -22,14 +22,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
->>>>>>> f7f3397b1f7dd2fd6ed624f30b3d08f43a0fb2c0
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,35 +2,35 @@ import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import React from "react";
 import { Grid } from "semantic-ui-react";
-
+import PostCard from '../components/PostCard';
 const Home = () => {
-  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-  if (data) {
-    console.log(data);
-  }
+  const {
+    loading,
+    data,
+  } = useQuery(FETCH_POSTS_QUERY);
   return (
     <Grid columns={3}>
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
-      {/* <Grid.Row>
+      <Grid.Row>
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
-          posts &&
-          posts.map((post) => (
+          data &&data.getPosts&&
+          data.getPosts.map((post) => (
             <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
               <PostCard post={post} />
             </Grid.Column>
           ))
         )}
-      </Grid.Row> */}
+      </Grid.Row>
     </Grid>
   );
 };
 
 const FETCH_POSTS_QUERY = gql`
-  query {
+  {
     getPosts {
       id
       username
